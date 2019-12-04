689908877c222b57a6bbdf3c94a0139ad2e8d71a
mrkaschenko1 <tenforever72@gmail.com>
2019-12-04 17:10:04 +0300

    made SearchPage

---
+++ book/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('search', views.BookSearch.as_view(), name='book_search'),
+]
---
+++ book/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+import requests
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+class BookSearch(View):
+    def get(self, request):
+        apiResponse = None
+        if (request.GET['query'] != ""):
+            query = request.GET['query'].replace(" ","%20")
+            apiResponse = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={query}').json()
+            print(apiResponse)
+        return render(request, 'book/book_search.html', {'book_list': apiResponse})
+
+    # @csrf_exempt
+    # def post(self, request):
+    #     return render(request, 'book/book_search.html', {})
