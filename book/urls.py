764fcd81ee472e2da99352f2925f52a1ee780fb3
mrkaschenko1 <tenforever72@gmail.com>
2019-12-11 04:15:14 +0300

    added shelves to main user page

--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    path('', login_required(views.show_books), name='book_list'),
     path('search/', login_required(views.BookSearch.as_view()), name='book_search'),
     re_path(r'^search/add/(?P<id>.{1,20})/$', login_required(views.AddToBookshelf.as_view()), name='book_add'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 import requests
@@ -6,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 from accounts.models import Profile
-from book.models import BookInfo
+from book.models import BookInfo, Shelf
 
 
 class BookSearch(View):
@@ -17,7 +18,7 @@ class BookSearch(View):
         apiResponse = None
         queryParam = request.GET.get('query')
         if (queryParam != "" and queryParam is not None):
-            query = request.GET['query'].replace(" ","%20")
+            query = request.GET['query'].replace(" ", "%20")
             apiResponse = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={query}').json()
             # print(apiResponse)
         return render(request, 'book/book_search.html', {'book_list': apiResponse, 'shelves': shelves})
@@ -34,7 +35,7 @@ class AddToBookshelf(View):
         try:
             book = BookInfo.objects.get(google_id=id)
         except BookInfo.DoesNotExist:
-            title = request.GET.get('title').replace("%20"," ")
+            title = request.GET.get('title').replace("%20", " ")
             book = BookInfo(google_id=id,
                             title=title,
                             authors=(",").join(book_info['volumeInfo']['authors']),
@@ -51,10 +52,35 @@ class AddToBookshelf(View):
             shelf.books.add(book)
             messages.success(request, f'Your book was added on {shelf_name} shelf!')
 
-
         print(profile.bio)
         return render(request, 'book/book_add.html', {'book': book_info})
 
-    # @csrf_exempt
-    # def post(self, request):
-    #     return render(request, 'book/book_search.html', {})
+# def get_active_shelf(name, current):
+#     return  "active" ? name == current : ""
+
+def show_books(request):
+    shelves = Shelf.objects.filter(profile=request.user.profile)
+    shelf_names = [shelf.name for shelf in shelves]
+    try:
+        current_shelf_name = request.GET.get('shelf').replace("%20", " ")
+    except AttributeError:
+        current_shelf_name = 'To read'
+    if current_shelf_name not in shelf_names:
+        raise Http404
+
+    shelf_objs = []
+    for name in shelf_names:
+        shelf_obj = {
+            "name": name,
+            "active": "active" if name==current_shelf_name else ""
+        }
+        shelf_objs.append(shelf_obj)
+
+    books = list(Shelf.objects.filter(name=current_shelf_name, profile=request.user.profile)\
+                                .first().books.all())
+    return render(request, 'book/book_list.html',
+                  {'shelves': shelf_objs,
+                   'books': books
+                   })
+    # shelves_names = ','.join([shelf.name for shelf in shelves])
+    # return HttpResponse(shelves_names)
